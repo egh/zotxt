e624d1ce99da504cdde336f58ce85cde9816b802
Erik Hetzner <egh@e6h.org>
2013-07-17 07:31:56 -0700

    fix encoding problem with extractcites

--- scripts/extractcites.py
+++ scripts/extractcites.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+import codecs
 import json
 import re
 import sys
 import urllib, urllib2
 
-f = open(sys.argv[1], 'r')
+f = codecs.open(sys.argv[1], 'r', encoding='utf-8')
 cite_re = re.compile(r'(?:@)[\w:\.#$%&_+?<>~/-]+', re.U)
 known_keys = set([])
 for line in f:
@@ -16,7 +17,7 @@ for line in f:
 cites = []
 for key in known_keys:
     try:
-        q = {'easykey' : key}
+        q = {'easykey' : key.encode('utf8')}
         encq = urllib.urlencode(q)
         cite = json.load(urllib2.urlopen("http://localhost:23119/zotxt/items?" + encq))[0]
         cite["id"] = key
