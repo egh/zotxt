7c40388ecba7e956e425264681493077b15b63f2
Erik Hetzner <egh@e6h.org>
2014-01-18 10:48:28 -0800

    add format tests

--- tests/test.rb
+++ tests/test.rb
@@ -138,6 +138,61 @@ class ZotxtTest < MiniTest::Unit::TestCase
     assert_equal 200, resp.status
   end
 
+  def test_format_bibtex
+    resp = @client.get(@item_url, {"key" => "0_4T8MCITQ", "format" => "bibtex"})
+    assert_equal 200, resp.status
+    assert_equal("""
+@article{doe_article_2006,
+	title = {Article},
+	volume = {6},
+	journal = {Journal of Generic Studies},
+	author = {Doe, John},
+	year = {2006},
+	pages = {33--34}
+}""", resp.body)
+  end
+
+  def test_format_bibliography
+    resp = @client.get(@item_url, {"key" => "0_4T8MCITQ", "format" => "bibliography"})
+    assert_equal 200, resp.status
+    results = JSON.parse(resp.body)
+    assert_equal({"text"=>"Doe, John. “Article.” Journal of Generic Studies 6 (2006): 33–34.", 
+                   "html"=>"""<div style=\"line-height: 1.35; padding-left: 2em; text-indent:-2em;\" class=\"csl-bib-body\">
+  <div class=\"csl-entry\">Doe, John. “Article.” <i>Journal of Generic Studies</i> 6 (2006): 33–34.</div>
+  <span class=\"Z3988\" title=\"url_ver=Z39.88-2004&amp;ctx_ver=Z39.88-2004&amp;rfr_id=info%3Asid%2Fzotero.org%3A2&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&amp;rft.genre=article&amp;rft.atitle=Article&amp;rft.jtitle=Journal%20of%20Generic%20Studies&amp;rft.volume=6&amp;rft.aufirst=John&amp;rft.aulast=Doe&amp;rft.au=John%20Doe&amp;rft.date=2006&amp;rft.pages=33-34&amp;rft.spage=33&amp;rft.epage=34\"></span>
+</div>""", 
+                   "key"=>"0_4T8MCITQ"}, results[0])
+  end
+
+  def test_format_easykey
+    resp = @client.get(@item_url, {"key" => "0_4T8MCITQ", "format" => "easykey"})
+    assert_equal 200, resp.status
+    results = JSON.parse(resp.body)
+    assert_equal "doe:2006article", results[0]
+  end
+
+  def test_format_key
+    resp = @client.get(@item_url, {"key" => "0_4T8MCITQ", "format" => "key"})
+    assert_equal 200, resp.status
+    results = JSON.parse(resp.body)
+    assert_equal "0_4T8MCITQ", results[0]
+  end
+
+  def test_format_json
+    resp = @client.get(@item_url, {"key" => "0_4T8MCITQ", "format" => "json"})
+    assert_equal 200, resp.status
+    results = JSON.parse(resp.body)
+    assert_equal({"id"=>2858,
+      "type"=>"article-journal",
+      "title"=>"Article", 
+      "container-title"=>"Journal of Generic Studies", 
+      "page"=>"33-34", 
+      "volume"=>"6",
+      "author"=>[{"family"=>"Doe", "given"=>"John"}], 
+      "issued"=>{"date-parts"=>[["2006"]]}
+                   }, results[0])
+  end
+
   def test_completion
     resp = @client.get(@complete_url, {"easykey" => "Doe"})
     results = JSON.parse(resp.body)
