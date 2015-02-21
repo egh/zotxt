7a604f9361f845ac559cdca4b9a3c40e720df2f9
Erik Hetzner <egh@e6h.org>
2015-02-20 19:27:54 -0800

    fix python code (flake8)

--- pandoc-zotxt/pandoczotxt/__init__.py
+++ pandoc-zotxt/pandoczotxt/__init__.py
@@ -9,58 +9,62 @@
 #
 # Foobar is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with Foobar.    If not, see <http://www.gnu.org/licenses/>.
 
-from pandocfilters import walk, Header, Str, elt
-import codecs
+from pandocfilters import walk, Str, elt
 import json
 import sys
 import tempfile
-import urllib, urllib2
+import urllib
+import urllib2
 
-MetaInlines = elt('MetaInlines',1)
+MetaInlines = elt('MetaInlines', 1)
+
 
 def toJSONFilter(filters=[], metafilters=[]):
-  doc = json.loads(sys.stdin.read())
-  if len(sys.argv) > 1:
-    format = sys.argv[1]
-  else:
-    format = ""
-  altered = doc
-  for action in filters:
-    altered = walk(altered, action, format, doc[0]['unMeta'])
-  for action in metafilters:
-    action(altered[0]['unMeta'])
-  json.dump(altered, sys.stdout)
+    doc = json.loads(sys.stdin.read())
+    if len(sys.argv) > 1:
+        format = sys.argv[1]
+    else:
+        format = ""
+        altered = doc
+    for action in filters:
+        altered = walk(altered, action, format, doc[0]['unMeta'])
+    for action in metafilters:
+        action(altered[0]['unMeta'])
+        json.dump(altered, sys.stdout)
 
 known_keys = set([])
 
+
 def extractCites(key, value, format, meta):
-  global known_keys
-  if key == "Cite":
-    for cite in value[0]:
-      known_keys.add(cite['citationId'])
+    global known_keys
+    if key == "Cite":
+        for cite in value[0]:
+            known_keys.add(cite['citationId'])
+
 
 def alterMetadata(meta):
-  global known_keys
-  cites = []
-  for citekey in known_keys:
-    try:
-      q = {'easykey' : citekey.encode('utf8')}
-      encq = urllib.urlencode(q)
-      cite = json.load(urllib2.urlopen("http://localhost:23119/zotxt/items?" + encq))[0]
-      cite["id"] = citekey
-      cites.append(cite)
-    except urllib2.HTTPError, e:
-      sys.stderr.write("error with %s : %s \n"%(citekey.encode('utf8'), e.read()))
-  tmpfile = tempfile.NamedTemporaryFile(suffix='.json', delete=False)
-  json.dump(cites, tmpfile, indent=2)
-  tmpfile.close()
-  meta['bibliography'] = MetaInlines([Str(tmpfile.name)])
+    global known_keys
+    cites = []
+    for citekey in known_keys:
+        try:
+            q = {'easykey': citekey.encode('utf8')}
+            encq = urllib.urlencode(q)
+            cite = json.load(urllib2.urlopen("http://localhost:23119/zotxt/items?" + encq))[0]
+            cite["id"] = citekey
+            cites.append(cite)
+        except urllib2.HTTPError, e:
+            sys.stderr.write("error with %s : %s \n" % (citekey.encode('utf8'), e.read()))
+    tmpfile = tempfile.NamedTemporaryFile(suffix='.json', delete=False)
+    json.dump(cites, tmpfile, indent=2)
+    tmpfile.close()
+    meta['bibliography'] = MetaInlines([Str(tmpfile.name)])
+
 
 def run():
-    toJSONFilter(filters=[extractCites], metafilters=[alterMetadata])
+        toJSONFilter(filters=[extractCites], metafilters=[alterMetadata])
